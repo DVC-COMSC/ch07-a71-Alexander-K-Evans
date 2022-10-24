79d28a41fd86ef81c2e3f7f1560a72f1c7891f0d
Alexander Evans <kingevans22202@gmail.com>
2022-10-24 13:41:12 -0700

    Complete

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-
 numbers = list(map(int, input().split()))
+print(numbers)
 
-# ******************************
-# Make your Code
-# ******************************
+avg = sum(numbers)/len(numbers)
 
+for i in range(len(numbers)):
+    calc = abs(avg - numbers[i])
+    print (f'{calc:.2f}', end=' ')
 
 # Use this statement to print out the list element. # Replace the variable 'dist' with your variable
 # print (f'{dist:.2f}', end=' ')
